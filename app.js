a9c80e7ed975eaa57fabb26ca976655c476ea825
grantibrees <grantibrees@gmail.com>
2020-06-24 13:45:00 -0600

    added art and initial structure

---
+++ app.js
@@ -0,0 +1,5 @@
+// Teleportation Circle, Chain Lightning, Wall of Force
+// Chain Lightning beats Teleportation Circle (TOO SLOW!)
+// Teleportation Circle beats Wall of Force (ESCAPED!)
+// Wall of Force beats Chain Lightning (BLOCKED!) 
+
